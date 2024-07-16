448f99e1f3d1b397c98151aa6d2a12a5dd5bc8af
Alan Vieira Martins <alanhorcrux@gmail.com>
2024-07-16 18:32:15 -0300

    chore: Add expo-font as a dependency and configure font loading in App.tsx

--- marketspace/App.tsx
+++ marketspace/App.tsx
@@ -1,12 +1,23 @@
 import { GluestackUIProvider, Text, Box, Center } from '@gluestack-ui/themed';
 import { config } from './config/gluestack-ui.config';
+import { useFonts } from 'expo-font';
+import { Karla_400Regular, Karla_700Bold } from '@expo-google-fonts/karla';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({Karla_400Regular,Karla_700Bold});
+
   return (
     <GluestackUIProvider config={config}>
-      <Center flex={1}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </Center>
+      {fontsLoaded ? (
+        <Center flex={1}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </Center>
+      ) : (
+        <Center flex={1}>
+          <Text>Loading...</Text>
+        </Center>
+      )}
     </GluestackUIProvider>
   );
 }
